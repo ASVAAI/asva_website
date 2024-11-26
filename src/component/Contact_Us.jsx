75b234491dfc89d82b0e355e2363e78a75064acf
viren040 <77960159+viren040@users.noreply.github.com>
2024-11-26 16:22:28 +0530

    Update Contact_Us.jsx

--- src/component/Contact_Us.jsx
+++ src/component/Contact_Us.jsx
@@ -21,7 +21,7 @@ function Contact() {
       return;
     }
   
-    fetch('https://wcd2dihotl.execute-api.eu-north-1.amazonaws.com', {
+    fetch('https://wcd2dihotl.execute-api.eu-north-1.amazonaws.com/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
